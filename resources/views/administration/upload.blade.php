c0bb3c842d45d2b4cd0bd478f9ad60dbef43dd65
Oliver <jasper.kontturi@gmail.com>
2024-04-14 06:53:05 +0300

    Fucking iis

--- resources/views/administration/upload.blade.php
+++ resources/views/administration/upload.blade.php
@@ -7,17 +7,24 @@
                 <form action="{{ route('admin.gallery.upload') }}" method="POST" enctype="multipart/form-data"
                     class="space-y-4">
                     @csrf
+                    @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                    <span class="text-sm text-red-600">Kiitos! {{ session()->get('success') }}</span>
+                    @endforeach
+                    @endif
                     @if (session()->has('success'))
-                        <span class="text-sm text-green-600">Kiitos! {{ session()->get('success') }}</span>
+                    <span class="text-sm text-green-600">Kiitos! {{ session()->get('success') }}</span>
                     @endif
                     <div class="mb-4">
                         <label for="photo" class="block text-sm font-medium text-gray-700">Valitse kuva</label>
-                        <input type="file" id="photo" name="photo" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black">
+                        <input type="file" id="photo" name="photo"
+                            class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black">
                     </div>
                     <input type="hidden" name="uploader" value="{{ Auth::user()->id }}">
                     <div class="mb-4">
                         <label for="category" class="block text-sm font-medium text-gray-700">Rooli</label>
-                        <select id="category" name="category" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black">
+                        <select id="category" name="category"
+                            class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black">
                             <option value="instagram" selected>Instagram</option>
                             <option value="facebook">Facebook</option>
                             <option value="verkkokauppa">Verkkokauppa</option>
@@ -26,14 +33,17 @@
                     </div>
                     <div class="mb-4">
                         <label for="title" class="block text-sm font-medium text-gray-700">Otsikko</label>
-                        <input type="text" id="title" name="title" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black">
+                        <input type="text" id="title" name="title"
+                            class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black">
                     </div>
                     <div class="mb-4">
                         <label for="description" class="block text-sm font-medium text-gray-700">Kuvaus</label>
-                        <textarea type="text" rows="3" id="description" name="description" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black"></textarea>
+                        <textarea type="text" rows="3" id="description" name="description"
+                            class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black"></textarea>
                     </div>
                     <div class="flex items-center justify-between">
-                        <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Lataa</button>
+                        <button type="submit"
+                            class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Lataa</button>
                     </div>
                 </form>
             </div>
